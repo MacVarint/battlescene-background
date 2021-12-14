7fdcf76cb3a2181d75e579d03f9a75832371d479
Code_Mac <80384615+MacVarint@users.noreply.github.com>
2021-12-14 12:44:59 +0100

    Added beaver

--- main.ts
+++ main.ts
@@ -1,5 +1,6 @@
 namespace SpriteKind {
     export const Background = SpriteKind.create()
+    export const prop = SpriteKind.create()
 }
 function setupBattleScene () {
     scene.setBackgroundImage(img`
@@ -128,10 +129,59 @@ function setupBattleScene () {
     Grasssprite.setPosition(80, 60)
     platforms = sprites.create(assets.image`Battle grass2`, SpriteKind.Background)
     platforms.setPosition(80, 60)
-    Cloud_sprite = sprites.create(assets.image`Clouds`, SpriteKind.Background)
-    Cloud_sprite.setPosition(80, 60)
+    generateBeaver()
 }
-let Cloud_sprite: Sprite = null
+function generateBeaver () {
+    Envirodant = sprites.create(assets.image`Clouds`, SpriteKind.Enemy)
+    Envirodant.setPosition(133, 71)
+    animation.runImageAnimation(
+    Envirodant,
+    assets.animation`Beaver`,
+    200,
+    true
+    )
+    envirodentAddition = sprites.create(img`
+        . . . . . b b b b b b . . . . . 
+        . . . b b d d d d d d b b . . . 
+        . . b b d d b b b b d d b b . . 
+        . e d b d b d d d d b d b d e . 
+        . f d b d d b b b b d d b d e . 
+        . f b d b b d d d d b b d b e . 
+        . f e d d d b b b b d d d e e . 
+        . f f e b d d d d d d b e e f . 
+        . f f e e e e e e e e e e e f . 
+        . f f e e e f e e e e e e e f . 
+        . f f e f e e e f f e e f e e f 
+        . f e e f e f e f e f e f e e e 
+        f f e e e e f e e f f e f f e e 
+        f e e e e f f e e e e e f f f e 
+        e e e e f f f e f e e e e f f f 
+        e e e e f f f e f e e e e f f f 
+        `, SpriteKind.prop)
+    envirodentAddition.setPosition(137, 85)
+    envirodentAddition2 = sprites.create(img`
+        . . . . . b b b b b b . . . . . 
+        . . . b b d d d d d d b b . . . 
+        . . b b d d b b b b d d b b . . 
+        . e d b d b d d d d b d b d e . 
+        . f d b d d b b b b d d b d e . 
+        . f b d b b d d d d b b d b e . 
+        . f e d d d b b b b d d d e e . 
+        . f f e b d d d d d d b e e f . 
+        . f f e e e e e e e e e e e f . 
+        . f f e e e f e e e e e e e f . 
+        . f f e f e e e f f e e f e e f 
+        . f e e f e f e f e f e f e e e 
+        f f e e e e f e e f f e f f e e 
+        f e e e e f f e e e e e f f f e 
+        e e e e f f f e f e e e e f f f 
+        e e e e f f f e f e e e e f f f 
+        `, SpriteKind.prop)
+    envirodentAddition2.setPosition(153, 85)
+}
+let envirodentAddition2: Sprite = null
+let envirodentAddition: Sprite = null
+let Envirodant: Sprite = null
 let platforms: Sprite = null
 let Grasssprite: Sprite = null
 setupBattleScene()
